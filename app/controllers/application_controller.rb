9a12a906d89a744a5956cb5d7918d6c040504ebf
Javier <65590523+javierc435@users.noreply.github.com>
2020-08-23 15:41:52 +0200

    Added Active_Storage feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  include Rails.application.routes.url_helpers
+
+  def index
+    @event = Event.first
+    cover_url = rails_blob_path(@event.cover, disposition: "attachment", only_path: true)
+  end
 end
